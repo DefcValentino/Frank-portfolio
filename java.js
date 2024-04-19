4dec41827906c0f6b190bfa7cec71b71101130c5
DefcValentino <164953300+DefcValentino@users.noreply.github.com>
2024-04-19 13:44:10 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,26 @@
+﻿
+const hamburgerMenu = document.querySelector('#navigation .nav-icon');
+const navContent = document.querySelector('#nav-content');
+const closeNavbutton = document.querySelector('#nav-content .close-btn');
+const navlinks = document.querySelectorAll('#nav-content nav ul li a');
+
+
+
+
+hamburgerMenu.addEventListener('click', () => {
+    navContent.classList.add('show');
+    document.body.style.overflow = "hidden";
+})
+
+closeNavbutton.addEventListener('click', () => {
+    navContent.classList.remove('show');
+    document.body.style.overflow = "initial";
+})
+
+navlinks.forEach( link => {
+    link.addEventListener('click', () => {
+        navContent.classList.remove('show');
+        document.body.style.overflow = "initial";
+    })
+})
+
